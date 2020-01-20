33950197a236ce61ade36dc59cfc13605a5ce6d7
Geographes ESR <59658732+geographesESR@users.noreply.github.com>
2020-01-20 10:34:34 +0100

    Update interurgences.groovy

--- scripts/interurgences.groovy
+++ scripts/interurgences.groovy
@@ -1,7 +1,18 @@
 import groovy.util.XmlParser
 import groovy.util.XmlSlurper
 
+//Remove old interurgences data
 def geojsonSante = new File("sante.geojson")
+def clean = ""
+for (line in geojsonSante){
+	if(!line.contains('"Id":"interurgence_')){
+		println line
+		clean += line
+	}
+}
+geojsonSante.text = clean
+
+//Adds new interurgences data
 def file = new File("INTER URGENCES.kml")
 def kml = new XmlSlurper().parse(file)
 assert kml
@@ -14,10 +25,10 @@ assert folder.Placemark.size() > 0
 
 for(placemark in folder.Placemark) {
 	def geojsonStr = ',{"type":"Feature","properties":{'
-	geojsonStr += '"Id":interurgence_"' + placemark.ExtendedData.Data[0].value.text() + '",'
-	geojsonStr += '"Secteur":"Justice",'
-	geojsonStr += '"Titre":"Urgences en grève : ' + placemark.name.text() + '",'
-	geojsonStr += '"Description":"' + placemark.ExtendedData.Data[1].value.text() + '",'
+	geojsonStr += '"Id":"interurgence_' + placemark.ExtendedData.Data[0].value.text().trim() + '",'
+	geojsonStr += '"Secteur":"Santé",'
+	geojsonStr += '"Titre":"Urgences en grève : ' + placemark.name.text().trim() + '",'
+	geojsonStr += '"Description":"' + placemark.ExtendedData.Data[1].value.text().trim() + '",'
 	geojsonStr += '"Type":"Grève",'
 	geojsonStr += '"Motif":"Hôpital en danger",'
 	geojsonStr += '"Debut":"",'
