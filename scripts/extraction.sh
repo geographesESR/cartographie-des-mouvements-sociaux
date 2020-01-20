220dedfb8f98781b07c04a4c3db1de9e28a959b1
Geographes ESR <59658732+geographesESR@users.noreply.github.com>
2020-01-20 11:55:07 +0100

    Update extraction.sh

--- scripts/extraction.sh
+++ scripts/extraction.sh
@@ -23,10 +23,8 @@ typeset -i indexFramaform=$(cat scripts/index.framaform)
 echo $indexFramaform
 
 #Write data from interurgences
-echo `sed '$ s/..$//' sante.geojson` > sante.geojson
 curl "https://www.google.com/maps/d/kml?mid=1QuZ2EogIgvffjcNC_-w4C22LUsgsUPoM&forcekml=1" > "INTER_URGENCES.kml" 
 bash apache-groovy-binary-3.0.0-rc-3/groovy-3.0.0-rc-3/bin/groovy scripts/interurgences.groovy
-echo "]}" >> sante.geojson
 rm "INTER_URGENCES.kml"
 
 #Read data.csv and put each line in the good geojson file
