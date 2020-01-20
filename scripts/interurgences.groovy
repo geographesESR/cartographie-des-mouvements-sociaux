4fd2372fe00e3b6effdd4fa8b48c9acd7ede68d8
Geographes ESR <59658732+geographesESR@users.noreply.github.com>
2020-01-20 11:36:21 +0100

    Update interurgences.groovy

--- scripts/interurgences.groovy
+++ scripts/interurgences.groovy
@@ -6,12 +6,16 @@ def geojsonSante = new File("sante.geojson")
 def clean = ""
 for (line in geojsonSante){
 	if(!line.contains('"Id":"interurgence_')){
-		clean += line + "\n"
+		clean += line+"\n"
 	}
 }
-geojsonSante.text = clean
+geojsonSante.text = clean - "\n]}"
 
 //Adds new interurgences data
+def empty = false
+if(geojsonSante.text == '{"type": "FeatureCollection","features": [\n'){
+	empty = true
+}
 def file = new File("INTER_URGENCES.kml")
 def kml = new XmlSlurper().parse(file)
 assert kml
@@ -23,7 +27,14 @@ assert folder.name.text().startsWith("SAU en grève")
 assert folder.Placemark.size() > 0
 
 for(placemark in folder.Placemark) {
-	def geojsonStr = ',{"type":"Feature","properties":{'
+	def geojsonStr = ''
+	if(empty){
+		empty = false
+	}
+	else{
+		geojsonStr += ','
+	}
+	geojsonStr += '{"type":"Feature","properties":{'
 	geojsonStr += '"Id":"interurgence_' + placemark.ExtendedData.Data[0].value.text().trim() + '",'
 	geojsonStr += '"Secteur":"Santé",'
 	geojsonStr += '"Titre":"Urgences en grève : ' + placemark.name.text().trim() + '",'
@@ -32,7 +43,7 @@ for(placemark in folder.Placemark) {
 	geojsonStr += '"Motif":"Hôpital en danger",'
 	geojsonStr += '"Debut":"",'
 	geojsonStr += '"Fin":"",'
-	geojsonStr += '"Source":"https://www.interurgences.fr/carte-de-france/"'
+	geojsonStr += '"Source":https://www.interurgences.fr/carte-de-france/"'
 	geojsonStr += '},"geometry":{"type":"Point","coordinates":['
 	geojsonStr += placemark.Point.coordinates.text().trim()
 	geojsonStr += ']}}\n'
